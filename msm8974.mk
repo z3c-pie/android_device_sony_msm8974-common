8ed0d523dd703c175ededbf418cc19f4a0efa920
Max Weffers <rcstar6696@gmail.com>
2017-11-24 12:51:11 +0100

    msm8974: hidl: Use lineages legacy camera device and provider
    Change-Id: I8c0e3ac873cda932a2e8deaa378501e280451ea5

--- msm8974.mk
+++ msm8974.mk
@@ -64,8 +64,8 @@ PRODUCT_PACKAGES += \
 		libGraphicBuffer
 
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.device@1.0-impl
+    android.hardware.camera.provider@2.4-impl-legacy \
+    camera.device@1.0-impl-legacy
 
 # Display
 PRODUCT_PACKAGES += \
