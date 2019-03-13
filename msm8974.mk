cde8dd9d977d4ba035277b7d8723f58d78f2b777
arian <arian.kulmer@web.de>
2019-03-13 20:49:30 +0100

    msm8974-common: Build Display HAL

--- msm8974.mk
+++ msm8974.mk
@@ -60,6 +60,11 @@ PRODUCT_PACKAGES += \
     libtilerenderer \
     libI420colorconvert
 
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.composer@2.1-impl
+
 # Ion
 PRODUCT_PACKAGES += \
     libion
