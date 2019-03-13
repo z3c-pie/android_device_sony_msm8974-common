56c92cbbe8cc4c54a15099954b03cd966cc1d64a
arian <arian.kulmer@web.de>
2019-03-13 21:40:09 +0100

    msm8974-common: lineagehw: Apply LineageOS rebrand

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -69,8 +69,8 @@ RED_LED_PATH := /sys/class/leds/led:rgb_red/brightness
 GREEN_LED_PATH := /sys/class/leds/led:rgb_green/brightness
 BLUE_LED_PATH := /sys/class/leds/led:rgb_blue/brightness
 
-# CM Hardware
-BOARD_HARDWARE_CLASS += device/sony/msm8974-common/cmhw
+# Lineage Hardware
+BOARD_HARDWARE_CLASS += device/sony/msm8974-common/lineagehw
 
 # Font
 EXTENDED_FONT_FOOTPRINT := true
