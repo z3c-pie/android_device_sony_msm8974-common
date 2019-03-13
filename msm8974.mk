6cff2cf589a00ab53b6a885bdc148bbacb507cb7
arian <arian.kulmer@web.de>
2019-03-13 21:01:52 +0100

    msm8974-common: Build memtrack

--- msm8974.mk
+++ msm8974.mk
@@ -102,6 +102,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     $(COMMON_PATH)/media_profiles.xml:system/etc/media_profiles.xml
 
+# Memtrack
+PRODUCT_PACKAGES += \
+    android.hardware.memtrack@1.0-impl
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.0-impl
