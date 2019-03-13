5b23cf0f133b94f40825c3bc4dc6e61805d54167
arian <arian.kulmer@web.de>
2019-03-13 21:02:37 +0100

    msm8974-common: Build USB

--- msm8974.mk
+++ msm8974.mk
@@ -161,6 +161,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     thermanager
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl
