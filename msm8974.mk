f243ca71da5aa1cf3ebe2b2d454bbc48c22e615a
arian <arian.kulmer@web.de>
2019-03-13 21:01:10 +0100

    msm8974-common: Build Sensors HAL

--- msm8974.mk
+++ msm8974.mk
@@ -149,6 +149,10 @@ PRODUCT_PACKAGES += \
     librmnetctl \
     libxml2
 
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl
+
 # Thermal management
 PRODUCT_PACKAGES += \
     thermanager
