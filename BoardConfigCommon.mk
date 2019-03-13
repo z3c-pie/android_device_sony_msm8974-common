c068715ce6b80adf9e44f78dc207b71f2f5d9f55
arian <arian.kulmer@web.de>
2019-03-13 22:51:25 +0100

    msm8974-common: Reorganize BoardConfig

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -14,39 +14,17 @@
 
 BOARD_VENDOR := sony
 
-# Use Snapdragon LLVM Compiler if available
-TARGET_USE_SDCLANG := true
-
-# Include path
-TARGET_SPECIFIC_HEADER_PATH += device/sony/msm8974-common/include
-
-TARGET_NO_BOOTLOADER := true
-TARGET_NO_RADIOIMAGE := true
-
-# Kernel properties
-ifeq (,$(filter $(TARGET_KERNEL_SOURCE),))
-  TARGET_KERNEL_SOURCE := kernel/sony/msm8974
-endif
-
-# use CAF variants
-BOARD_USES_QCOM_HARDWARE := true
-TARGET_USES_QCOM_BSP := true
-TARGET_QCOM_MEDIA_VARIANT := caf-msm8974
-
-# Platform
-TARGET_BOOTLOADER_BOARD_NAME := MSM8974
-TARGET_BOARD_PLATFORM := msm8974
-
 # Architecture
 TARGET_ARCH := arm
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+TARGET_BOARD_PLATFORM := msm8974
+TARGET_BOOTLOADER_BOARD_NAME := MSM8974
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := krait
 TARGET_CUSTOM_DTBTOOL := dtbToolLineage
-
-# Defines for HW subsystems
--include $(PLATFORM_PATH)/hardware/*/BoardConfig.mk
 
 # Audio
 BOARD_USES_ALSA_AUDIO := true
@@ -55,6 +33,11 @@ AUDIO_FEATURE_ENABLED_EXTN_POST_PROC := true
 
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
+
+# CAF variants
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_USES_QCOM_BSP := true
+TARGET_QCOM_MEDIA_VARIANT := caf-msm8974
 
 # Camera
 TARGET_PROVIDES_CAMERA_HAL := true
@@ -67,16 +50,10 @@ TARGET_USES_MEDIA_EXTENSIONS := true
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_SHOW_PERCENTAGE := true
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
-BACKLIGHT_PATH :=/sys/class/leds/lcd-backlight/brightness
+BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
 RED_LED_PATH := /sys/class/leds/led:rgb_red/brightness
 GREEN_LED_PATH := /sys/class/leds/led:rgb_green/brightness
 BLUE_LED_PATH := /sys/class/leds/led:rgb_blue/brightness
-
-# Lineage Hardware
-BOARD_HARDWARE_CLASS += device/sony/msm8974-common/lineagehw
-
-# Font
-EXTENDED_FONT_FOOTPRINT := true
 
 # Dexpreopt
 ifeq ($(HOST_OS),linux)
@@ -87,6 +64,9 @@ ifeq ($(HOST_OS),linux)
     endif
   endif
 endif
+
+# Font
+EXTENDED_FONT_FOOTPRINT := true
 
 # GPS
 USE_DEVICE_SPECIFIC_GPS := true
@@ -104,9 +84,6 @@ TARGET_USES_GRALLOC1_ADAPTER := true
 SF_VSYNC_EVENT_PHASE_OFFSET_NS := 5000000
 VSYNC_EVENT_PHASE_OFFSET_NS := 7500000
 
-# Security patch level
-VENDOR_SECURITY_PATCH := 2016-05-01
-
 # Shader cache config options
 # Maximum size of the  GLES Shaders that can be cached for reuse.
 # Increase the size if shaders of size greater than 12KB are used.
@@ -118,11 +95,25 @@ MAX_EGL_CACHE_KEY_SIZE := 12*1024
 MAX_EGL_CACHE_SIZE := 2048*1024
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS := 0x02000000U
 
+# HW subsystems
+-include $(PLATFORM_PATH)/hardware/*/BoardConfig.mk
+
+# Include path
+TARGET_SPECIFIC_HEADER_PATH += device/sony/msm8974-common/include
+
 # Init configuration for init_sony
 BOARD_USES_INIT_SONY := true
 
+# Kernel properties
+ifeq (,$(filter $(TARGET_KERNEL_SOURCE),))
+  TARGET_KERNEL_SOURCE := kernel/sony/msm8974
+endif
+
 # Lights HAL
 TARGET_PROVIDES_LIBLIGHT := true
+
+# Lineage Hardware
+BOARD_HARDWARE_CLASS += device/sony/msm8974-common/lineagehw
 
 # Power
 TARGET_HAS_LEGACY_POWER_STATS := true
@@ -131,6 +122,10 @@ TARGET_USES_INTERACTION_BOOST := true
 
 # RIL
 TARGET_RIL_VARIANT := caf
+BOARD_PROVIDES_LIBRIL := true
+
+# Security patch level
+VENDOR_SECURITY_PATCH := 2016-05-01
 
 # SELinux
 include device/qcom/sepolicy-legacy/sepolicy.mk
@@ -138,11 +133,11 @@ include device/qcom/sepolicy-legacy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += \
     device/sony/msm8974-common/sepolicy
 
-# RIL
-BOARD_PROVIDES_LIBRIL := true
-
-# Treble
-DEVICE_MANIFEST_FILE := device/sony/msm8974-common/manifest.xml
+# Snapdragon LLVM Compiler
+TARGET_USE_SDCLANG := true
 
 # Sony Open Devices build barrier
  PRODUCT_PLATFORM_SOD := true
+
+# Vendor Interface Manifest
+DEVICE_MANIFEST_FILE := device/sony/msm8974-common/manifest.xml
