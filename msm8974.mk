5537c0eb1d5f5227f3f82867b06d19569062df7d
arian <arian.kulmer@web.de>
2019-03-14 15:33:49 +0100

    msm8974-common: Fix wrong formatting in msm8974.mk

--- msm8974.mk
+++ msm8974.mk
@@ -24,8 +24,8 @@ COMMON_PATH := device/sony/msm8974-common
 
 # AID/fs configs
 PRODUCT_PACKAGES += \
-   fs_config_files \
-	 liboemaids
+    fs_config_files \
+    liboemaids
 
 # Apps
 PRODUCT_PACKAGES += \
@@ -58,14 +58,14 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-		android.hardware.bluetooth@1.0-service
+    android.hardware.bluetooth@1.0-service
 
 # Camera (stock blobs)
 PRODUCT_PACKAGES += \
-		camera.qcom \
-		libshim_camera \
-		libshim_cald \
-		libshims_signal \
+    camera.qcom \
+    libshim_camera \
+    libshim_cald \
+    libshims_signal \
     libshims_idd \
     libsonycamera
 
@@ -88,16 +88,16 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
-		android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.graphics.composer@2.1-impl \
-		android.hardware.graphics.composer@2.1-service \
-		android.hardware.renderscript@1.0-impl
+    android.hardware.graphics.composer@2.1-service \
+    android.hardware.renderscript@1.0-impl
 
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-		android.hardware.drm@1.0-service
+    android.hardware.drm@1.0-service
 
 # Dumpstate
 PRODUCT_PACKAGES += \
@@ -110,16 +110,16 @@ PRODUCT_PACKAGES += \
 # GNSS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.1-impl-qti \
-		android.hardware.gnss@1.1-service-qti
+    android.hardware.gnss@1.1-service-qti
 
 # GPS
 PRODUCT_PACKAGES += \
     libgnss \
     libgnsspps\
-		libgps.utils \
-		libloc_api_v02 \
-		libloc_core \
-		libloc_ds_api \
+    libgps.utils \
+    libloc_api_v02 \
+    libloc_core \
+    libloc_ds_api \
     libloc_pla \
     liblocation_api
 
@@ -130,7 +130,7 @@ PRODUCT_COPY_FILES += \
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
-		android.hardware.health@2.0-service
+    android.hardware.health@2.0-service
 
 # Ion
 PRODUCT_PACKAGES += \
@@ -139,7 +139,7 @@ PRODUCT_PACKAGES += \
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
-		android.hardware.keymaster@3.0-service
+    android.hardware.keymaster@3.0-service
 
 # Lights
 PRODUCT_PACKAGES += \
@@ -147,7 +147,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
-		android.hardware.light@2.0-service
+    android.hardware.light@2.0-service
 
 # Media profile
 PRODUCT_COPY_FILES += \
@@ -159,17 +159,17 @@ PRODUCT_COPY_FILES += \
 # Memtrack
 PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
-		android.hardware.memtrack@1.0-service
+    android.hardware.memtrack@1.0-service
 
 # Net
 PRODUCT_PACKAGES += \
-		android.system.net.netd@1.0 \
-		netutils-wrapper-1.0
+    android.system.net.netd@1.0 \
+    netutils-wrapper-1.0
 
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.0-impl \
-		android.hardware.nfc@1.0-service
+    android.hardware.nfc@1.0-service
 
 # Omx
 PRODUCT_PACKAGES += \
@@ -229,7 +229,7 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-		android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service
 
 # Thermal
 PRODUCT_PACKAGES += \
@@ -237,7 +237,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
-		android.hardware.thermal@1.0-service
+    android.hardware.thermal@1.0-service
 
 # TimeKeep for managing time offsets w.r.t. RTC
 PRODUCT_PACKAGES += \
